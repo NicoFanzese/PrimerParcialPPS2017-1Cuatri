7c47854d7afeb731a439e62aefa07f113599d835
Nicolás Franzese <franzesens@gmail.com>
2017-04-25 21:59:24 -0300

    Modificación de funciones de Trivia
    Se modifican las funciones de la trivia, para que lleven los resultados
    hasta el final de la aplicación. Luego estos se muestran en una pagina
    de resultados.

--- Trivia/src/pages/home/home.ts
+++ Trivia/src/pages/home/home.ts
@@ -3,12 +3,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 //var email = "";
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  private resumen= "";
+  private acierto = 0;
 
   constructor(public navCtrl: NavController) {
     
@@ -19,23 +21,56 @@ export class HomePage {
     document.getElementById('Pregunta1').style.display='inline'; 
   }
 
-  pasarApregunta2(respuesta: String) { 
-    console.log(respuesta);
+  pasarApregunta2(respuesta) { 
+    //console.log(respuesta);
+
+    if (respuesta == "unoUno"){
+          this.resumen += " <b>¿En qué año se fue a la B Independiente?: </b> 15 de junio de 2013<br>" ; 
+          this.acierto += 1;        
+       }else if (respuesta == "dosUno"){
+          this.resumen += " <b>¿En qué año se fue a la B Independiente?: </b> 25 de Enero de 2014<br>";          
+       }else if (respuesta == "tresUno"){
+          this.resumen += " <b>¿En qué año se fue a la B Independiente?: </b> 8 de Agosto de 2013<br>";                    
+    }
+
     document.getElementById('Pregunta1').style.display='none'; 
     document.getElementById('Pregunta2').style.display='inline';
   }
 
-  pasarApregunta3(respuesta: String) { 
+  pasarApregunta3(respuesta) { 
+     if (respuesta == "unoDos"){
+           this.resumen += " <b>Campeón del Torneo de Primera División Dr. Ramón Carrillo: </b> Independiente<br>";
+        }else if(respuesta == "dosDos"){
+           this.resumen += " <b>Campeón del Torneo de Primera División Dr. Ramón Carrillo: </b> River<br>";
+        }else if(respuesta == "tresDos"){
+           this.resumen += " <b>Campeón del Torneo de Primera División Dr. Ramón Carrillo: </b> Racing<br>";
+           this.acierto += 1;
+      }
+
     document.getElementById('Pregunta2').style.display='none'; 
     document.getElementById('Pregunta3').style.display='inline';
   }
 
-  finalizar(respuesta: String) { 
+  finalizar(respuesta) { 
+    if (respuesta == "unoTres"){
+           this.resumen += " <b>Jugador que arañó a Abbondanzieri: </b> 'La Gata' fernandez<br>";           
+        }else if(respuesta== "dosTres"){
+           this.resumen += " <b>Jugador que arañó a Abbondanzieri: </b> 'El muñeco' Gallardo<br>";
+           this.acierto += 1;
+        }else if(respuesta == "tresTres"){
+           this.resumen += " <b>Jugador que arañó a Abbondanzieri: </b> 'El Caco' Centurión<br>";
+      }
+
+    document.getElementById('ResultadoRespuestas').innerHTML = this.resumen; 
+    document.getElementById('Total').innerHTML = this.acierto + " / 3";
+
     document.getElementById('Pregunta3').style.display='none'; 
     document.getElementById('Resultado').style.display='inline';
   }
 
   jugar() { 
+    this.resumen = "";
+    this.acierto = 0;
     document.getElementById('Resultado').style.display='none'; 
     document.getElementById('Menu').style.display='inline';
   }
